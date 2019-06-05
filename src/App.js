d731da8abd4b971a1b9490a926a603c87f6a3a9c
ethan6077 <ethan6077@gmail.com>
2019-06-05 18:01:05 +1000

    Tile drag and drop done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Frame from './components/Frame';
 import './styles/App.scss';
 
-function App() {
-  return (
-    <div className="app">
-      <Frame />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Frame />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import classNames from 'classnames';
 
-function Tile(props) {
-  const { value, moveTile } = props;
-  return (
-    <div
-      onClick={() => moveTile(value)}
-      className={classNames({
-      'tile1': value === 1,
-      'tile2': value === 2,
-      'tile3': value === 3,
-      'tile4': value === 4,
-      'tile5': value === 5,
-      'tile6': value === 6,
-      'tile7': value === 7,
-      'tile8': value === 8,
-      'tile9': value === 9,
-      })}
-    >
-      {value}
-    </div>
-  );
+class Tile extends React.Component {
+  _onDragStart = (e) => {
+    const { value } = this.props;
+    console.log('_onDragStart')
+    e.dataTransfer.dropEffect = "move";
+    e.dataTransfer.setData('value', value);
+  }
+
+  _onDragOver = (e) => {
+    e.preventDefault();
+  }
+
+  _onDrop = (e) => {
+    const { value, moveTile } = this.props;
+    console.log('_onDrop');
+    if (value === 9) {
+      const valueToDrop = parseInt(e.dataTransfer.getData('value'));
+      console.log(valueToDrop);
+      moveTile(valueToDrop);
+    }
+  }
+
+  render() {
+    const { value } = this.props;
+    return (
+      <div
+        draggable={value === 9 ? false : true}
+        onDragStart={this._onDragStart}
+        onDragOver={value === 9 ? this._onDragOver : null}
+        onDrop={value === 9 ? this._onDrop : null}
+        className={classNames({
+        'tile1': value === 1,
+        'tile2': value === 2,
+        'tile3': value === 3,
+        'tile4': value === 4,
+        'tile5': value === 5,
+        'tile6': value === 6,
+        'tile7': value === 7,
+        'tile8': value === 8,
+        'tile9': value === 9,
+        })}
+      >
+        {value}
+      </div>
+    );
+  }
 }
 
 export default Tile;
