fc798b400375a9f72682524079fd6b50b518935a
ethan6077 <ethan6077@gmail.com>
2019-06-05 15:57:57 +1000

    added colors for tiles

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import classNames from 'classnames';
 
 function Tile(props) {
   const { value } = props;
   return (
-    <div className="tile">
+    <div className={classNames({
+      'tile1': value === 1,
+      'tile2': value === 2,
+      'tile3': value === 3,
+      'tile4': value === 4,
+      'tile5': value === 5,
+      'tile6': value === 6,
+      'tile7': value === 7,
+      'tile8': value === 8,
+      'tile9': value === 9,
+    })}>
       {value}
     </div>
   );
