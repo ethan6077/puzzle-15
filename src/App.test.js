1ca40214e076242567004a255ec2964295371645
ethan6077 <ethan6077@gmail.com>
2019-06-05 22:35:04 +1000

    App test done

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import Header from './components/Header';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('there is no header when the app starts', () => {
+  const appWrapper = shallow(<App />);
+  expect(appWrapper.contains(<Header />)).toBe(false);
+});
+
+it('renders Header when the puzzle resolved', () => {
+  const appWrapper = shallow(<App />);
+  appWrapper.instance().changePuzzleStatus();
+  expect(appWrapper.contains(<Header />)).toBe(true);
 });
--- src/components/Frame.test.js
+++ src/components/Frame.test.js
@@ -8,15 +8,15 @@ it('renders without crashing', () => {
 
 it('changePuzzleStatus should be called once when tileValues are equal to doneValues', () => {
   const changePuzzleStatusMock = jest.fn();
-  const propertyPageWrapper = shallow(<Frame changePuzzleStatus={changePuzzleStatusMock} isPuzzleDone={false} />);
-  propertyPageWrapper.setState({ tileValues: [1,2,3,4,5,6,7,8,9] });
+  const frameWrapper = shallow(<Frame changePuzzleStatus={changePuzzleStatusMock} isPuzzleDone={false} />);
+  frameWrapper.setState({ tileValues: [1,2,3,4,5,6,7,8,9] });
   expect(changePuzzleStatusMock).toHaveBeenCalledTimes(1);
 });
 
 it('changePuzzleStatus should be called once when the last tile moved to the right place', () => {
   const changePuzzleStatusMock = jest.fn();
-  const propertyPageWrapper = shallow(<Frame changePuzzleStatus={changePuzzleStatusMock} isPuzzleDone={false} />);
-  propertyPageWrapper.setState({ tileValues: [1,2,3,4,5,6,7,9,8] });
-  propertyPageWrapper.instance().moveTile(8);
+  const frameWrapper = shallow(<Frame changePuzzleStatus={changePuzzleStatusMock} isPuzzleDone={false} />);
+  frameWrapper.setState({ tileValues: [1,2,3,4,5,6,7,9,8] });
+  frameWrapper.instance().moveTile(8);
   expect(changePuzzleStatusMock).toHaveBeenCalledTimes(1);
 });
